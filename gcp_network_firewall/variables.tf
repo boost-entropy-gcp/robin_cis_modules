f234170a9b7b731bbf820ae1d8168ebdd35aac10
Terence Kam <t.kam@f5.com>
2021-03-17 22:23:24 +0900

    update vars

--- gcp_network_firewall/variables.tf
+++ gcp_network_firewall/variables.tf
@@ -8,6 +8,11 @@ variable "network" {
   type        = string
 }
 
+variable "network2" {
+  description = "A reference (self_link) to the VPC network to apply firewall rules to"
+  type        = string
+}
+
 variable "public_subnetwork" {
   description = "A reference (self_link) to the public subnetwork of the network"
   type        = string
